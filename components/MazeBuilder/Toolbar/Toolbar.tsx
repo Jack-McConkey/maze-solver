f42eefd805e49ccaec90a386438212e9c5b461df
Jack-McConkey <103947884+Jack-McConkey@users.noreply.github.com>
2022-10-26 21:34:09 +0100

    minor style changes & added headings for toolbar

--- components/MazeBuilder/Toolbar/Toolbar.tsx
+++ components/MazeBuilder/Toolbar/Toolbar.tsx
@@ -18,27 +18,33 @@ export default function Toolbar({
 }: ToolbarProps) {
     return (
         <div className={styles.container}>
-            <div className={styles["secondary-container"]}>
-                <Button
-                    btnText="Wall"
-                    handleClick={() => selectTool("wall")}
-                    btnStyle={currentTool === "wall" ? "selected" : "accent"}
-                />
-                <Button
-                    btnText="Start"
-                    handleClick={() => selectTool("start")}
-                    btnStyle={currentTool === "start" ? "selected" : "accent"}
-                />
-                <Button
-                    btnText="End"
-                    handleClick={() => selectTool("end")}
-                    btnStyle={currentTool === "end" ? "selected" : "accent"}
-                />
+            <div>
+                <h2 className={styles["heading"]}>Tools</h2>
+                <div className={styles["secondary-container"]}>
+                    <Button
+                        btnText="Wall"
+                        handleClick={() => selectTool("wall")}
+                        btnStyle={currentTool === "wall" ? "selected" : "accent"}
+                    />
+                    <Button
+                        btnText="Start"
+                        handleClick={() => selectTool("start")}
+                        btnStyle={currentTool === "start" ? "selected" : "accent"}
+                    />
+                    <Button
+                        btnText="End"
+                        handleClick={() => selectTool("end")}
+                        btnStyle={currentTool === "end" ? "selected" : "accent"}
+                    />
+                </div>
             </div>
-            <div className={styles["secondary-container"]}>
-                <Button btnText="Generate" handleClick={generateMaze} btnStyle={"generate"} />
-                <Button btnText="Search" handleClick={searchMaze} btnStyle={"search"} />
-                <Button btnText="Clear" handleClick={clearMaze} btnStyle={"clear"} />
+            <div>
+                <h2 className={styles["heading"]}>Actions</h2>
+                <div className={styles["secondary-container"]}>
+                    <Button btnText="Search" handleClick={searchMaze} btnStyle={"search"} />
+                    <Button btnText="Generate" handleClick={generateMaze} btnStyle={"generate"} />
+                    <Button btnText="Clear" handleClick={clearMaze} btnStyle={"clear"} />
+                </div>
             </div>
         </div>
     );
